db0f3debfe4c12b4d61e0a05e5e833818eb61c9c
zooperdan <dan.thoresen@gmail.com>
2022-04-01 21:09:05 +0200

    Added example project

---
+++ Example/LÖVE/src/conf.lua
@@ -0,0 +1,12 @@
+function love.conf(t)
+	t.title = "AtlasMaker - Dungeon example"
+	t.version = "11.4"
+	t.identity = "atlasmaker-dungeon-example"
+	t.appendidentity = true
+	t.window.width = 640
+	t.window.height = 512
+	t.console = false
+	t.window.fullscreen = false
+	t.window.fullscreentype = "desktop"
+	t.window.fsaa = 0
+end
